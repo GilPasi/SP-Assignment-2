f9d02320f9eca9f5194ebc63beaffe30e97805fa
Gil_Pasi <95695418+GilPasi@users.noreply.github.com>
2022-12-10 10:32:36 +0200

    Game-abstract class created

---
+++ Tic Tac Toe/src/Players.java
@@ -0,0 +1,15 @@
+/*System-Programming : Assignment 2
+ *Authors: Yulia Moshan 319565610
+ *			Gil Pasi    206500936 */
+enum Players{
+	X(true),O(false);
+	
+	char sign;
+	
+	Players (boolean isX){
+		if(isX)
+			sign = 'X';
+		else
+			sign = 'O';
+	}
+}
